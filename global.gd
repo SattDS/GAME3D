8150f0549728e1425fe9bfbea62e333a4b613043
SattDS <sedykh.de@gmail.com>
2026-04-26 12:23:23 +0300

    1
    Co-Authored-By: MaxWarAxe <113930738+maxwaraxe@users.noreply.github.com>

---
+++ global.gd
@@ -0,0 +1,2 @@
+extends Node
+var G = Vector3(0,-100,0)
---
+++ player.gd
@@ -0,0 +1,48 @@
+extends CharacterBody3D
+@export var camera: Camera3D
+@export var sensitivity: float = 0.007
+@export var speed: float = 100
+
+@export var jump_force = 10000000
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		rotate_y(event.relative.x * -sensitivity)
+		camera.rotate_x(event.relative.y * -sensitivity)
+		camera.rotation.x = clamp(camera.rotation.x,deg_to_rad(-90),deg_to_rad(90))
+
+func _physics_process(delta: float) -> void:
+	
+	
+	
+	var input_direction = Vector3.ZERO
+	
+	if Input.is_action_pressed("forward"):
+		input_direction.z = -1
+		
+		
+	if Input.is_action_pressed("back"):
+		input_direction.z = 1
+		
+	if Input.is_action_pressed("left"):
+		input_direction.x = -1
+		
+	if Input.is_action_pressed("right"):
+		input_direction.x = 1
+		
+	var direction = (transform.basis * input_direction)
+	velocity.x = direction.x * speed
+	velocity.z = direction.z * speed
+	
+	if !is_on_floor():
+		velocity += Global.G * delta
+		
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_force
+	print(velocity)
+	
+	move_and_slide()
+	
